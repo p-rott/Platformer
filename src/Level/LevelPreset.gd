87c0be5102b256368d624a819dbfe32b9f870bb6
paul <paul@rotthu.es>
2021-01-20 12:41:00 +0100

    added pause menu to levels

--- src/Level/LevelPreset.gd
+++ src/Level/LevelPreset.gd
@@ -8,6 +8,7 @@ const LIMIT_RIGHT = 2000
 const LIMIT_BOTTOM = 950
 onready var player = $Player
 onready var playerSpawn = $ControlNodes/PlayerSpawn
+onready var pauseMenu = $CanvasLayer/PauseMenu
 
 var attemptStartTime : int
 var attemptEndTime : int
@@ -44,23 +45,3 @@ func playerDeath():
 
 func playerGoal():
 	attemptEndTime = OS.get_ticks_msec()
-
-"""
-
-#Camera limits
-const LIMIT_LEFT = -315
-const LIMIT_TOP = -250
-const LIMIT_RIGHT = 955
-const LIMIT_BOTTOM = 690
-
-func _ready():
-	for child in get_children():
-		if child is Player:
-			var camera = child.get_node("Camera")
-			camera.limit_left = LIMIT_LEFT
-			camera.limit_top = LIMIT_TOP
-			camera.limit_right = LIMIT_RIGHT
-			camera.limit_bottom = LIMIT_BOTTOM
-		elif child is FallingPlatform:
-
-"""
--- src/UserInterface/PauseMenu.gd
+++ src/UserInterface/PauseMenu.gd
@@ -18,3 +18,13 @@ func _on_ResumeButton_pressed():
 
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		var tree = get_tree()
+		tree.paused = not tree.paused
+		if tree.paused:
+			open()
+		else:
+			close()
+		get_tree().set_input_as_handled()
